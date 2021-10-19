977cae58ea74bdcfca11bd225fe8929670578f82
Michal Privoznik <mprivozn@redhat.com>
2021-10-19 14:28:05 +0200

    qemu: Start QEMU in preconfig phase whenever possible
    QEMU is capable of accepting parts of its configuration via
    monitor. This is referred to as a preconfig phase. If used, QEMU
    starts, parses the command line and just before the machine is
    created it waits for additional commands on the monitor. To exit
    this phase the 'exit-preconfig' command should be issued. Using
    preconfig has a great advantage - one can query the partial
    configuration, but at the same time not all commands are allowed
    (e.g. device_add). Nevertheless, this is going to be very useful
    in some cases.
    
    Even though, the preconfig phase is not going to be used in all
    cases, let's enable it regardless to simplify future code. For
    cases where preconfig is not needed it'll be a NOP.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/qemu/qemu_command.c
+++ src/qemu/qemu_command.c
@@ -10738,6 +10738,9 @@ qemuBuildCommandLine(virDomainObj *vm,
 
     virCommandAddArg(cmd, "-S"); /* freeze CPUs during startup */
 
+    if (virQEMUCapsGet(qemuCaps, QEMU_CAPS_EXIT_PRECONFIG))
+        virCommandAddArg(cmd, "-preconfig");
+
     if (qemuBuildMasterKeyCommandLine(cmd, priv) < 0)
         return NULL;
 
