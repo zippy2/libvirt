ca9c479ce67b27c6ae667d53b8b034794935ba43
Michal Privoznik <mprivozn@redhat.com>
2021-10-19 10:43:41 +0200

    qemu_capabilities: Add QEMU_CAPS_EXIT_PRECONFIG capability
    This new capability tracks whether QEMU has exit-preconfig
    monitor command. Please note, that the command was introduced in
    QEMU commit v3.0.0-rc0~124^2~4 but just before release it was
    marked experimental (commit v3.0.0-rc1~21^2~3). Only recently it
    was made stable again.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/qemu/qemu_capabilities.c
+++ src/qemu/qemu_capabilities.c
@@ -768,6 +768,7 @@ VIR_ENUM_IMPL(virQEMUCaps,
               /* 495 */
               "blockdev-mirror.target-is-zero", /* QEMU_CAPS_BLOCKDEV_MIRROR_TARGET_IS_ZERO */
               "object-monitor-qmp", /* QEMU_CAPS_OBJECT_MONITOR_QMP */
+              "exit-preconfig", /* QEMU_CAPS_EXIT_PRECONFIG */
     );
 
 
@@ -1298,6 +1299,7 @@ struct virQEMUCapsStringFlags virQEMUCapsCommands[] = {
     { "blockdev-set-active", QEMU_CAPS_BLOCKDEV_SET_ACTIVE },
     { "qom-list-get", QEMU_CAPS_QOM_LIST_GET },
     { "query-accelerators", QEMU_CAPS_QUERY_ACCELERATORS },
+    { "exit-preconfig", QEMU_CAPS_EXIT_PRECONFIG },
 };
 
 struct virQEMUCapsStringFlags virQEMUCapsObjectTypes[] = {
