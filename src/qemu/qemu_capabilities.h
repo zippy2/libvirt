0af458ebcb835a48ed522349b74d0a9f011b3c03
Michal Privoznik <mprivozn@redhat.com>
2021-10-19 10:43:41 +0200

    qemu_capabilities: Add QEMU_CAPS_EXIT_PRECONFIG capability
    This new capability tracks whether QEMU has exit-preconfig
    monitor command. Please note, that the command was introduced in
    QEMU commit v3.0.0-rc0~124^2~4 but just before release it was
    marked experimental (commit v3.0.0-rc1~21^2~3). Only recently it
    was made stable again.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/qemu/qemu_capabilities.h
+++ src/qemu/qemu_capabilities.h
@@ -750,6 +750,7 @@ typedef enum { /* virQEMUCapsFlags grouping marker for syntax-check */
     QEMU_CAPS_ARM_SMMUV3_CMDQV, /* arm-smmuv3.cmdqv */
     QEMU_CAPS_IOTHREAD_POLL_WEIGHT, /* -object iothread.poll-weight */
     QEMU_CAPS_WIN_DMP_GUEST_AWARE, /* 'win-dmp' is offered only to a guest that can use it */
+    QEMU_CAPS_EXIT_PRECONFIG, /* exit-preconfig QMP command */
 
     QEMU_CAPS_LAST /* this must always be the last item */
 } virQEMUCapsFlags;
