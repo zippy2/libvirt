61e5302ca50e7ad1c50b7686670876e6f57be2ba
Michal Privoznik <mprivozn@redhat.com>
2021-10-20 10:13:17 +0200

    qemu_monitor: Introduce exit-preconfig
    This commit introduces a wrapper for 'exit-preconfig' monitor
    command.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/qemu/qemu_monitor.c
+++ src/qemu/qemu_monitor.c
@@ -1413,6 +1413,15 @@ qemuMonitorSetCapabilities(qemuMonitor *mon)
 
 
 int
+qemuMonitorExitPreconfig(qemuMonitor *mon)
+{
+    QEMU_CHECK_MONITOR(mon);
+
+    return qemuMonitorJSONExitPreconfig(mon);
+}
+
+
+int
 qemuMonitorStartCPUs(qemuMonitor *mon)
 {
     QEMU_CHECK_MONITOR(mon);
