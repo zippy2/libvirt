47c2c81159ac2d3494c75b55342d90f63bc0b1db
Michal Privoznik <mprivozn@redhat.com>
2021-11-09 10:39:00 +0100

    qemu_monitor: Introduce qemuMonitorGetHotpluggableCPUs
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/qemu/qemu_monitor.c
+++ src/qemu/qemu_monitor.c
@@ -1766,6 +1766,19 @@ qemuMonitorGetCPUInfo(qemuMonitor *mon,
 }
 
 
+int
+qemuMonitorGetHotpluggableCPUs(qemuMonitor *mon,
+                               struct qemuMonitorQueryHotpluggableCpusEntry **entries,
+                               size_t *nentries)
+{
+    VIR_DEBUG("entries=%p nentries=%p", entries, nentries);
+
+    QEMU_CHECK_MONITOR(mon);
+
+    return qemuMonitorJSONGetHotpluggableCPUs(mon, entries, nentries);
+}
+
+
 /**
  * qemuMonitorGetCpuHalted:
  *
