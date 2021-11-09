8a1169b701897086566ea528927bdc1327f4cd7a
Michal Privoznik <mprivozn@redhat.com>
2021-11-09 11:01:08 +0100

    qemuMonitorSetNumaNode
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/qemu/qemu_monitor.c
+++ src/qemu/qemu_monitor.c
@@ -4609,3 +4609,19 @@ qemuMonitorAnnounceSelf(qemuMonitor *mon,
 
     return qemuMonitorJSONAnnounceSelf(mon, device, initial, max, rounds, step);
 }
+
+int
+qemuMonitorSetNumaNode(qemuMonitor *mon,
+                       unsigned int node,
+                       int socket,
+                       int die,
+                       int core,
+                       int thread)
+{
+    VIR_DEBUG("node=%u socket=%d die=%d core=%d thread=%d",
+              node, socket, die, core, thread);
+
+    QEMU_CHECK_MONITOR(mon);
+
+    return qemuMonitorJSONSetNumaNode(mon, node, socket, die, core, thread);
+}
--- src/qemu/qemu_monitor.h
+++ src/qemu/qemu_monitor.h
@@ -1965,7 +1965,6 @@ qemuMonitorDisplayReload(qemuMonitor *mon,
                          const char *type,
                          bool tlsCerts);
 
-
 int
 qemuMonitorSnapshotSave(qemuMonitor *mon,
                         const char *jobname,
@@ -2007,3 +2006,11 @@ qemuMonitorAnnounceSelf(qemuMonitor *mon,
                         unsigned int max,
                         unsigned int rounds,
                         unsigned int step);
+
+int
+qemuMonitorSetNumaNode(qemuMonitor *mon,
+                       unsigned int node,
+                       int socket,
+                       int die,
+                       int core,
+                       int thread);
