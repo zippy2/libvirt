4e98b1787f09c82cc5ac35ec311aba32368ce871
Michal Privoznik <mprivozn@redhat.com>
2021-11-09 11:01:08 +0100

    qemuMonitorSetNumaNode
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/qemu/qemu_monitor.h
+++ src/qemu/qemu_monitor.h
@@ -1650,7 +1650,6 @@ qemuMonitorDisplayReload(qemuMonitor *mon,
                          const char *type,
                          bool tlsCerts);
 
-
 int
 qemuMonitorSnapshotSave(qemuMonitor *mon,
                         const char *jobname,
@@ -1675,3 +1674,11 @@ int
 qemuMonitorBlockdevSetActive(qemuMonitor *mon,
                              const char *nodename,
                              bool active);
+
+int
+qemuMonitorSetNumaNode(qemuMonitor *mon,
+                       unsigned int node,
+                       int socket,
+                       int die,
+                       int core,
+                       int thread);
