db49b24939540d9ed4229d56ad043b965df4fd40
Michal Privoznik <mprivozn@redhat.com>
2022-01-31 19:23:08 +0100

    network
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/network/bridge_driver.c
+++ src/network/bridge_driver.c
@@ -1202,7 +1202,6 @@ networkDnsmasqConfTFTP(virBuffer *buf,
 
 int
 networkDnsmasqConfContents(virNetworkObj *obj,
-                           const char *pidfile,
                            char **configstr,
                            char **hostsfilestr,
                            dnsmasqContext *dctx,
@@ -1549,7 +1548,7 @@ networkBuildDhcpDaemonCommandLine(virNetworkDriverState *driver,
 
     virNetworkObjSetDnsmasqPid(obj, -1);
 
-    if (networkDnsmasqConfContents(obj, pidfile, &configstr, &hostsfilestr,
+    if (networkDnsmasqConfContents(obj, &configstr, &hostsfilestr,
                                    dctx, dnsmasq_caps) < 0)
         return -1;
     if (!configstr)
@@ -1579,6 +1578,8 @@ networkBuildDhcpDaemonCommandLine(virNetworkDriverState *driver,
     virCommandAddArgFormat(cmd, "--leasefile-ro");
     virCommandAddArgFormat(cmd, "--dhcp-script=%s", leaseshelper_path);
     virCommandAddEnvPair(cmd, "VIR_BRIDGE_NAME", def->bridge);
+    virCommandSetPidFile(cmd, pidfile);
+    virCommandDaemonize(cmd);
 
     *cmdout = g_steal_pointer(&cmd);
     return 0;
@@ -1655,14 +1656,6 @@ networkStartDhcpDaemon(virNetworkDriverState *driver,
 
     if (virCommandRun(cmd, NULL) < 0)
         return -1;
-
-    /*
-     * There really is no race here - when dnsmasq daemonizes, its
-     * leader process stays around until its child has actually
-     * written its pidfile. So by time virCommandRun exits it has
-     * waitpid'd and guaranteed the proess has started and written a
-     * pid
-     */
 
     if (virPidFileRead(cfg->pidDir, def->name, &dnsmasqPid) < 0)
         return -1;
