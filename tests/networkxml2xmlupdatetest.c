7fceacc857122023e50be9917d21c1c94675b986
Michal Privoznik <mprivozn@redhat.com>
2022-09-20 15:36:05 +0200

    test
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- tests/networkxml2xmlupdatetest.c
+++ tests/networkxml2xmlupdatetest.c
@@ -355,6 +355,15 @@ mymain(void)
                  "dns-host-modify-not-existing",
                  "nat-network-dns-hosts",
                  VIR_NETWORK_UPDATE_COMMAND_MODIFY);
+    DO_TEST_FAIL("insert-incomplete-host",
+                 "dns-host-gateway-incomplete",
+                 "nat-network-dns-hosts",
+                 VIR_NETWORK_UPDATE_COMMAND_MODIFY_OR_ADD_FIRST);
+    DO_TEST("insert-host",
+            "dns-host-pudding",
+            "nat-network-dns-hosts",
+            "nat-network-dns-more-hosts",
+            VIR_NETWORK_UPDATE_COMMAND_MODIFY_OR_ADD_FIRST);
 
 
     section = VIR_NETWORK_SECTION_DNS_TXT;
