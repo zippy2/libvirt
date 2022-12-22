57dd2cb0c00d9a764d1ab1d75c6ef444cf3a0239
Michal Privoznik <mprivozn@redhat.com>
2022-12-22 16:58:21 +0100

    tests: Use virDirOpenSorted()
    Since the directory content is not returned in a sorted manner it
    may be harder than necessary to find a particular test case. For
    instance domaincapstest runs individual test cases in "random"
    order. At the first glance it may seem that we are missing a
    particular version test. Well, now that we have
    virDirOpenSorted() the test cases can run in more predictable
    way.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- tests/testutilsqemu.c
+++ tests/testutilsqemu.c
@@ -499,7 +499,7 @@ testQemuGetLatestCaps(void)
     g_autoptr(virDir) dir = NULL;
     int rc;
 
-    if (virDirOpen(&dir, TEST_QEMU_CAPS_PATH) < 0)
+    if (virDirOpenSorted(&dir, TEST_QEMU_CAPS_PATH) < 0)
         return NULL;
 
     while ((rc = virDirRead(dir, &ent, TEST_QEMU_CAPS_PATH)) > 0) {
@@ -570,7 +570,7 @@ testQemuCapsIterate(const char *suffix,
         return -1;
     }
 
-    if (virDirOpen(&dir, TEST_QEMU_CAPS_PATH) < 0)
+    if (virDirOpenSorted(&dir, TEST_QEMU_CAPS_PATH) < 0)
         return -1;
 
     while ((rc = virDirRead(dir, &ent, TEST_QEMU_CAPS_PATH)) > 0) {
