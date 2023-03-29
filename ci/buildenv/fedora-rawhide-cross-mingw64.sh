dc6b6a9129cd7843a06fc68845ff9e3d6b81c6a4
Michal Privoznik <mprivozn@redhat.com>
2023-03-29 16:03:31 +0200

    ci: Regenerate files
    This enables our CI infrastructure to run tests with help of wine
    on mingw cross builds.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- ci/buildenv/fedora-rawhide-cross-mingw64.sh
+++ ci/buildenv/fedora-rawhide-cross-mingw64.sh
@@ -53,7 +53,33 @@ function install_buildenv() {
         mingw64-libxml2 \
         mingw64-pkg-config \
         mingw64-portablexdr \
-        mingw64-readline
+        mingw64-readline \
+        wine
+    mkdir -p /usr/local/share/meson/cross
+    printf "[binaries]\n\
+c = '/usr/bin/x86_64-w64-mingw32-gcc'\n\
+cpp = '/usr/bin/x86_64-w64-mingw32-g++'\n\
+fortran = '/usr/bin/x86_64-w64-mingw32-gfortran'\n\
+rust = ['rustc', '--target', 'x86_64-pc-windows-msvc', '-C', 'linker=/usr/bin/x86_64-w64-mingw32-gcc']\n\
+ar = '/usr/bin/x86_64-w64-mingw32-ar'\n\
+pkgconfig = '/usr/bin/x86_64-w64-mingw32-pkg-config'\n\
+ranlib = '/usr/bin/x86_64-w64-mingw32-ranlib'\n\
+strip = '/usr/bin/x86_64-w64-mingw32-strip'\n\
+windres = '/usr/bin/x86_64-w64-mingw32-windres'\n\
+dlltool = '/usr/bin/x86_64-w64-mingw32-dlltool'\n\
+libgcrypt-config = '/usr/x86_64-w64-mingw32/sys-root/mingw/bin/libgcrypt-config'\n\
+glib-mkenums = '/usr/x86_64-w64-mingw32/sys-root/mingw/bin/glib-mkenums'\n\
+exe_wrapper = 'wine'\n\
+\n\
+[properties]\n\
+root = '/usr/x86_64-w64-mingw32/sys-root/mingw'\n\
+needs_exe_wrapper = true\n\
+\n\
+[host_machine]\n\
+system = 'windows'\n\
+cpu_family = 'x86_64'\n\
+cpu = 'x86_64'\n\
+endian = 'little'\n" > /usr/local/share/meson/cross/x86_64-w64-mingw32
     rpm -qa | sort > /packages.txt
     mkdir -p /usr/libexec/ccache-wrappers
     ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/x86_64-w64-mingw32-cc
@@ -67,4 +93,4 @@ export NINJA="/usr/bin/ninja"
 export PYTHON="/usr/bin/python3"
 
 export ABI="x86_64-w64-mingw32"
-export MESON_OPTS="--cross-file=/usr/share/mingw/toolchain-mingw64.meson"
+export MESON_OPTS="--cross-file=x86_64-w64-mingw32"
