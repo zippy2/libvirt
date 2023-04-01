be25fac5fdcb8461234d2c2ca27d14026347d37e
Michal Privoznik <mprivozn@redhat.com>
2023-04-01 13:24:27 +0200

    tests: Move virArchFromHost() into a proper mock
    There is a bunch of tests that transitively call
    virArchFromHost(). So far, we have a mock implemented in
    testutils.c which is then statically linked to each test binary
    and later, when the test is ran and dynamic linker loads
    libvirt.so we hope it resolves virArchFromHost symbol to the one
    provided by the test binary itself rather than the one from
    libvirt.so itself. And on ELF systems, this works because ELF is
    designed this way. But on PE systems (like mingw/wine) the binary
    is never considered for symbol resolution.
    
    Therefore, move the function out and into its own mock library.
    But since we want tests to affect the value returned by the
    function, let's turn the global variable into an envvar. This way
    we don't have to come up with a needlessly complicated way of
    linking and virTestSetHostArch() is then a dummy wrapper around
    g_setenv().
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- tests/qemucapabilitiestest.c
+++ tests/qemucapabilitiestest.c
@@ -228,4 +228,5 @@ mymain(void)
 }
 
 VIR_TEST_MAIN_PRELOAD(mymain,
-                      VIR_TEST_MOCK("domaincaps"))
+                      VIR_TEST_MOCK("domaincaps"),
+                      VIR_TEST_MOCK("virarch"))
