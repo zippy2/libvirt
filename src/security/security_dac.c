00a8d10379ce4368b88ef0e0bef601530f6b659f
Michal Privoznik <mprivozn@redhat.com>
2023-07-14 10:16:29 +0200

    security: Drop some G_GNUC_UNUSED attributes
    Some variables are annotated with G_GNUC_UNUSED attribute so that
    compiles knows to not complain about unused variable. Well, some
    variables are annotated and then used. Drop the G_GNUC_UNUSED
    attribute in such cases.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/security/security_dac.c
+++ src/security/security_dac.c
@@ -1655,7 +1655,7 @@ struct _virSecurityDACChardevCallbackData {
 
 static int
 virSecurityDACRestoreChardevCallback(virDomainDef *def,
-                                     virDomainChrDef *dev G_GNUC_UNUSED,
+                                     virDomainChrDef *dev,
                                      void *opaque)
 {
     struct _virSecurityDACChardevCallbackData *data = opaque;
@@ -2005,7 +2005,7 @@ virSecurityDACRestoreAllLabel(virSecurityManager *mgr,
 
 static int
 virSecurityDACSetChardevCallback(virDomainDef *def,
-                                 virDomainChrDef *dev G_GNUC_UNUSED,
+                                 virDomainChrDef *dev,
                                  void *opaque)
 {
     struct _virSecurityDACChardevCallbackData *data = opaque;
