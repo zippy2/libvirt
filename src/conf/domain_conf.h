22f067f189912623b90a63c888101e7b65cf53fe
Michal Privoznik <mprivozn@redhat.com>
2023-09-18 15:14:56 +0200

    _virDomainInputDef: model
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/conf/domain_conf.h
+++ src/conf/domain_conf.h
@@ -1601,7 +1601,7 @@ typedef enum {
 struct _virDomainInputDef {
     virDomainInputType type;
     int bus;
-    int model; /* virDomainInputModel */
+    virDomainInputModel model;
     struct {
         char *evdev;
         virDomainInputSourceGrab grab;
