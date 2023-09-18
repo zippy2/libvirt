9ab1b3823d7b138cf0c596cb75b4dd5c322cd67f
Michal Privoznik <mprivozn@redhat.com>
2023-09-18 13:27:02 +0200

    _virDomainInputDef: type
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/qemu/qemu_cgroup.c
+++ src/qemu/qemu_cgroup.c
@@ -435,6 +435,11 @@ qemuSetupInputCgroup(virDomainObj *vm,
     case VIR_DOMAIN_INPUT_TYPE_EVDEV:
         return qemuCgroupAllowDevicePath(vm, dev->source.evdev,
                                          VIR_CGROUP_DEVICE_RW, false);
+    case VIR_DOMAIN_INPUT_TYPE_MOUSE:
+    case VIR_DOMAIN_INPUT_TYPE_TABLET:
+    case VIR_DOMAIN_INPUT_TYPE_KBD:
+    case VIR_DOMAIN_INPUT_TYPE_LAST:
+        break;
     }
 
     return ret;
@@ -455,6 +460,11 @@ qemuTeardownInputCgroup(virDomainObj *vm,
     case VIR_DOMAIN_INPUT_TYPE_EVDEV:
         return qemuCgroupDenyDevicePath(vm, dev->source.evdev,
                                         VIR_CGROUP_DEVICE_RWM, false);
+    case VIR_DOMAIN_INPUT_TYPE_MOUSE:
+    case VIR_DOMAIN_INPUT_TYPE_TABLET:
+    case VIR_DOMAIN_INPUT_TYPE_KBD:
+    case VIR_DOMAIN_INPUT_TYPE_LAST:
+        break;
     }
 
     return 0;
