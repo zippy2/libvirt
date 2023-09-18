daf2cb63781ec794015d841406009bb04fb48cf4
Michal Privoznik <mprivozn@redhat.com>
2023-09-18 12:55:14 +0200

    virDomainPanicModel
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/conf/domain_conf.h
+++ src/conf/domain_conf.h
@@ -2837,7 +2837,7 @@ bool virDomainMemoryIsVirtioModel(const virDomainMemoryDef *def);
 
 
 typedef enum {
-    VIR_DOMAIN_PANIC_MODEL_DEFAULT,
+    VIR_DOMAIN_PANIC_MODEL_DEFAULT = 0,
     VIR_DOMAIN_PANIC_MODEL_ISA,
     VIR_DOMAIN_PANIC_MODEL_PSERIES,
     VIR_DOMAIN_PANIC_MODEL_HYPERV,
@@ -2848,7 +2848,7 @@ typedef enum {
 } virDomainPanicModel;
 
 struct _virDomainPanicDef {
-    int model; /* virDomainPanicModel */
+    virDomainPanicModel model;
     virDomainDeviceInfo info;
 };
 
