47baa91fd18bb50cc3e456e701f2019b658abfb8
Michal Privoznik <mprivozn@redhat.com>
2023-09-18 15:22:37 +0200

    _virDomainInputDef::bus
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/conf/domain_conf.h
+++ src/conf/domain_conf.h
@@ -1568,7 +1568,7 @@ typedef enum {
 
 struct _virDomainInputDef {
     virDomainInputType type;
-    int bus;
+    virDomainInputBus bus;
     virDomainInputModel model;
     struct {
         char *evdev;
--- src/qemu/qemu_command.c
+++ src/qemu/qemu_command.c
@@ -4256,7 +4256,7 @@ qemuBuildInputCommandLine(virCommand *cmd,
         } else {
             g_autoptr(virJSONValue) props = NULL;
 
-            switch ((virDomainInputBus) input->bus) {
+            switch (input->bus) {
             case VIR_DOMAIN_INPUT_BUS_USB:
                 if (!(props = qemuBuildInputUSBDevProps(def, input)))
                     return -1;
