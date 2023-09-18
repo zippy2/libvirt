454c7b52fb34083199cdc233b4dcb93e4a6a021b
Michal Privoznik <mprivozn@redhat.com>
2023-09-18 15:22:37 +0200

    _virDomainInputDef::bus
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/conf/domain_conf.h
+++ src/conf/domain_conf.h
@@ -1596,7 +1596,7 @@ typedef enum {
 
 struct _virDomainInputDef {
     virDomainInputType type;
-    int bus;
+    virDomainInputBus bus;
     virDomainInputModel model;
     struct {
         char *evdev;
