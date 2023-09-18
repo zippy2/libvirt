7679fbc4d2b891da8e9e49c20937907941809e47
Michal Privoznik <mprivozn@redhat.com>
2023-09-18 15:14:56 +0200

    _virDomainInputDef: model
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/conf/domain_conf.c
+++ src/conf/domain_conf.c
@@ -11357,7 +11357,6 @@ virDomainInputDefParseXML(virDomainXMLOption *xmlopt,
     VIR_XPATH_NODE_AUTORESTORE(ctxt)
     virDomainInputDef *def;
     g_autofree char *bus = NULL;
-    g_autofree char *model = NULL;
     xmlNodePtr source = NULL;
 
     def = g_new0(virDomainInputDef, 1);
@@ -11365,7 +11364,6 @@ virDomainInputDefParseXML(virDomainXMLOption *xmlopt,
     ctxt->node = node;
 
     bus = virXMLPropString(node, "bus");
-    model = virXMLPropString(node, "model");
 
     if (virXMLPropEnum(node, "type",
                        virDomainInputTypeFromString,
@@ -11374,11 +11372,10 @@ virDomainInputDefParseXML(virDomainXMLOption *xmlopt,
         goto error;
     }
 
-    if (model &&
-        ((def->model = virDomainInputModelTypeFromString(model)) < 0 ||
-         def->model == VIR_DOMAIN_INPUT_MODEL_DEFAULT)) {
-        virReportError(VIR_ERR_CONFIG_UNSUPPORTED,
-                       _("unknown input model '%1$s'"), model);
+    if (virXMLPropEnum(node, "model",
+                       virDomainInputModelTypeFromString,
+                       VIR_XML_PROP_NONZERO,
+                       &def->model) < 0) {
         goto error;
     }
 
