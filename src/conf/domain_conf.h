b471a42cffc2410a7a50c87b900efdabd9dacb65
Michal Privoznik <mprivozn@redhat.com>
2023-09-18 12:55:14 +0200

    virDomainPanicModel
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/conf/domain_conf.h
+++ src/conf/domain_conf.h
@@ -2735,7 +2735,7 @@ G_DEFINE_AUTOPTR_CLEANUP_FUNC(virDomainMemoryDef, virDomainMemoryDefFree);
 
 
 typedef enum {
-    VIR_DOMAIN_PANIC_MODEL_DEFAULT,
+    VIR_DOMAIN_PANIC_MODEL_DEFAULT = 0,
     VIR_DOMAIN_PANIC_MODEL_ISA,
     VIR_DOMAIN_PANIC_MODEL_PSERIES,
     VIR_DOMAIN_PANIC_MODEL_HYPERV,
@@ -2746,7 +2746,7 @@ typedef enum {
 } virDomainPanicModel;
 
 struct _virDomainPanicDef {
-    int model; /* virDomainPanicModel */
+    virDomainPanicModel model;
     virDomainDeviceInfo info;
 };
 
