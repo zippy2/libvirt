9bbe382b14d9787b9dc54c24b37fde525a3e0a58
Michal Privoznik <mprivozn@redhat.com>
2024-06-14 09:02:56 +0200

    qemu_capabilities: Introduce QEMU_CAPS_SEV_GUEST_LEGACY_VM_TYPE
    This capability tracks whether the sev-guest object has
    'legacy-vm-type' attribute. It's going to be important later.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/qemu/qemu_capabilities.h
+++ src/qemu/qemu_capabilities.h
@@ -749,6 +749,7 @@ typedef enum { /* virQEMUCapsFlags grouping marker for syntax-check */
     /* 500 */
     QEMU_CAPS_ARM_SMMUV3_CMDQV, /* arm-smmuv3.cmdqv */
     QEMU_CAPS_IOTHREAD_POLL_WEIGHT, /* -object iothread.poll-weight */
+    QEMU_CAPS_SEV_GUEST_LEGACY_VM_TYPE, /* -object sev-guest.legacy-vm-type= */
 
     QEMU_CAPS_LAST /* this must always be the last item */
 } virQEMUCapsFlags;
