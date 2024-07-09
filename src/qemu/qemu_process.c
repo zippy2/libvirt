e0e0eb0b5dcf6c2d9645e3a53cfe187e231b9136
Michal Privoznik <mprivozn@redhat.com>
2024-07-09 11:44:04 +0200

    qemu: Don't reset error when stopping caps probing QEMU process
    If QEMU dies while we are querying its capabilities [1],
    qemuProcessQMPStop() is called. In here, virResetLastError() is
    called which clears any error message set previously. In most
    cases this is going to be "Unable to read from monitor:
    Connection reset by peer" but that's far better than leaving caps
    probing with no error. Because if we did reset an error, then
    we'll get:
    
      # virsh start fedora
      error: Failed to start domain 'fedora'
      error: An error occurred, but the cause is unknown
    
      # virsh domcapabilities
      error: failed to get emulator capabilities
      error: An error occurred, but the cause is unknown
    
    And after this commit we'll get at least:
    
      virsh # start fedora
      error: Failed to start domain 'fedora'
      error: Unable to read from monitor: Connection reset by peer
    
      virsh # domcapabilities
      error: failed to get emulator capabilities
      error: Unable to read from monitor: Connection reset by peer
    
    1: this can happen easily if QEMU has a bug, or its installation
    is broken. I've seen an instance where on Fedora 40 system QEMU
    RPM from RHEL-9 was installed. And since QEMU loads its modules
    dynamically, due to this mismatch it was aborting in
    qmp_qom_list_types().
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/qemu/qemu_process.c
+++ src/qemu/qemu_process.c
@@ -10167,7 +10167,6 @@ qemuProcessQMPStop(qemuProcessQMP *proc)
     if (proc->pid != 0) {
         VIR_DEBUG("Killing QMP caps process %lld", (long long)proc->pid);
         virProcessKillPainfully(proc->pid, true);
-        virResetLastError();
         proc->pid = 0;
     }
 
