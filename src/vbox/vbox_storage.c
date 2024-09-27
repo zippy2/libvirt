41b2911d87af215eb18554d9938d2830f624fc37
Michal Privoznik <mprivozn@redhat.com>
2024-09-27 13:49:07 +0200

    vbox: Add 7.1
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/vbox/vbox_storage.c
+++ src/vbox/vbox_storage.c
@@ -877,8 +877,10 @@ virStorageDriver *vboxGetStorageDriver(uint32_t uVersion)
     /* Install gVBoxAPI according to the vbox API version.
      * Return -1 for unsupported version.
      */
-    if (uVersion >= 7000000 && uVersion < 7000004) {
+    if (uVersion >= 7000000 && uVersion < 7001000) {
         vbox70InstallUniformedAPI(&gVBoxAPI);
+    } else if (uVersion >= 7001000 && uVersion < 7002000) {
+        vbox71InstallUniformedAPI(&gVBoxAPI);
     } else {
         return NULL;
     }
--- src/vbox/vbox_uniformed_api.h
+++ src/vbox/vbox_uniformed_api.h
@@ -562,3 +562,4 @@ virDomainPtr vboxDomainLookupByUUID(virConnectPtr conn,
 
 /* Version specified functions for installing uniformed API */
 void vbox70InstallUniformedAPI(vboxUniformedAPI *pVBoxAPI);
+void vbox71InstallUniformedAPI(vboxUniformedAPI *pVBoxAPI);
