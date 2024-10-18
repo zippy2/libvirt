32181850e71e79866852dfb7016340bf861de63b
Michal Privoznik <mprivozn@redhat.com>
2024-10-18 09:24:35 +0200

    ble
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- tests/qemumonitorjsontest.c
+++ tests/qemumonitorjsontest.c
@@ -1640,7 +1640,7 @@ testQemuMonitorJSONqemuMonitorJSONGetMigrationStats(const void *opaque)
 {
     const testGenericData *data = opaque;
     virDomainXMLOption *xmlopt = data->xmlopt;
-    qemuMonitorMigrationStats stats;
+    qemuMonitorMigrationStats stats = { 0 };
     qemuMonitorMigrationStats expectedStats = { 0 };
     g_autofree char *error = NULL;
     g_autoptr(qemuMonitorTest) test = NULL;
@@ -1687,7 +1687,6 @@ testQemuMonitorJSONqemuMonitorJSONGetMigrationStats(const void *opaque)
         return -1;
     }
 
-    memset(&stats, 0, sizeof(stats));
     if (qemuMonitorJSONGetMigrationStats(qemuMonitorTestGetMonitor(test),
                                          &stats, &error) < 0)
         return -1;
