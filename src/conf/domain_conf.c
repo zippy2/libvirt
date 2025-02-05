6e64634c060e446ec8b207150690494987591934
Michal Privoznik <mprivozn@redhat.com>
2025-02-05 13:48:21 +0100

    lib: Drop needless ret variables
    This patch was generated using the following spatch:
    
      @@
      type T;
      constant C;
      expression f;
      identifier ret;
      @@
      (
      -T ret = C;
      |
      -T ret;
      )
       ... when != ret
           when strict
      (
      -ret = f;
      -return ret;
      +return f;
      |
      -return ret;
      +return C;
      )
    
    Basically, if there's a variable declared at the beginning of a
    function and the variable is not changed until the very last
    moment, just before return, such variable is useless.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/conf/domain_conf.c
+++ src/conf/domain_conf.c
@@ -3955,11 +3955,7 @@ virDomainPanicDefFree(virDomainPanicDef *panic)
 virDomainLoaderDef *
 virDomainLoaderDefNew(void)
 {
-    virDomainLoaderDef *def = NULL;
-
-    def = g_new0(virDomainLoaderDef, 1);
-
-    return def;
+    return g_new0(virDomainLoaderDef, 1);
 }
 
 void
