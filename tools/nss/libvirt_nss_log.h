7c0d6423dda28253714a66b463ddbae65f30eb0f
Michal Privoznik <mprivozn@redhat.com>
2025-06-18 08:49:29 +0200

    nss: Move logging into a separate file
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

---
+++ tools/nss/libvirt_nss_log.h
@@ -0,0 +1,38 @@
+/*
+ * libvirt_nss_log: Logging for Name Service Switch plugin
+ *
+ * Copyright (C) 2025 Red Hat, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+typedef enum {
+    NSS_DEBUG,
+    NSS_ERROR,
+} nssLogPriority;
+
+#define DEBUG(...) \
+    nssLog(NSS_DEBUG, __FUNCTION__, __LINE__, __VA_ARGS__)
+
+#define ERROR(...) \
+    nssLog(NSS_ERROR, __FUNCTION__, __LINE__, __VA_ARGS__)
+
+void
+nssLog(nssLogPriority prio,
+       const char *func,
+       int linenr,
+       const char *fmt, ...) __attribute__ ((format(printf, 4, 5)));
