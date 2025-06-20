a3424b350385bb74bd84afaca6127d4e6ec326c8
Michal Privoznik <mprivozn@redhat.com>
2025-06-20 12:50:20 +0200

    qemu_command: geenerate -sandbox only after devices
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/qemu/qemu_command.c
+++ src/qemu/qemu_command.c
@@ -11075,9 +11075,6 @@ qemuBuildCommandLine(virDomainObj *vm,
                                  NULLSTR_EMPTY(qemuxmlns->env[i].value));
     }
 
-    if (qemuBuildSeccompSandboxCommandLine(cmd, cfg, qemuCaps) < 0)
-        return NULL;
-
     if (qemuBuildPanicCommandLine(cmd, def, qemuCaps) < 0)
         return NULL;
 
@@ -11098,6 +11095,9 @@ qemuBuildCommandLine(virDomainObj *vm,
         return NULL;
 
     if (qemuBuildAsyncTeardownCommandLine(cmd, def, qemuCaps) < 0)
+        return NULL;
+
+    if (qemuBuildSeccompSandboxCommandLine(cmd, cfg, qemuCaps) < 0)
         return NULL;
 
     if (cfg->logTimestamp)
