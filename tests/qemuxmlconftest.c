61292039a2f50ee6752eb5f2141333cb49cb5539
Michal Privoznik <mprivozn@redhat.com>
2025-07-01 16:01:28 +0200

    qemuxmlconftest: Use virusbmock
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- tests/qemuxmlconftest.c
+++ tests/qemuxmlconftest.c
@@ -3110,4 +3110,5 @@ VIR_TEST_MAIN_PRELOAD(mymain,
                       VIR_TEST_MOCK("virrandom"),
                       VIR_TEST_MOCK("qemucpu"),
                       VIR_TEST_MOCK("virnuma"),
-                      VIR_TEST_MOCK("virhostdev"))
+                      VIR_TEST_MOCK("virhostdev"),
+                      VIR_TEST_MOCK("virusb"))
