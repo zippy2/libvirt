2492c0049d56e885db50b6d309920948e86237e9
Michal Privoznik <mprivozn@redhat.com>
2025-07-23 15:54:33 +0200

    security_dac: Drop needles typecast in virSecurityDACGenLabel()
    Inside of virSecurityDACGenLabel() there's a switch() statement
    and the variable it uses is typecasted to virDomainSeclabelType.
    Well, as of v7.10.0-rc1~26 the variable is already of that type
    rendering the typecast needless. Drop it.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/security/security_dac.c
+++ src/security/security_dac.c
@@ -2435,7 +2435,7 @@ virSecurityDACGenLabel(virSecurityManager *mgr,
         return rc;
     }
 
-    switch ((virDomainSeclabelType)seclabel->type) {
+    switch (seclabel->type) {
     case VIR_DOMAIN_SECLABEL_STATIC:
         if (seclabel->label == NULL) {
             virReportError(VIR_ERR_INTERNAL_ERROR,
