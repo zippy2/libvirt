zippy2/libvirt

commit 32f7db0989e47319ce585d05af32193244e1223a

vmx: Support super wide SCSI bus
Since its 6.7 release, vSphere allows up to 64 units on a SCSI
bus [1]. The release version translates to virtualHW_version 13
and thus if we are dealing with sufficiently new version we can
enable the feature.

1: https://configmax.vmware.com/guest?vmwareproduct=vSphere&release=vSphere%206.7&categories=1-0

Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=1738392
Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/vmx/vmx.c
@@ -1476,6 +1476,8 @@ virVMXParseConfig(virVMXContext *ctx,
                          "4 or higher but found %lld"),
                        virtualHW_version);
         goto cleanup;
+    } else if (virtualHW_version >= 13) {
+        def->scsiBusMaxUnit = SCSI_SUPER_WIDE_BUS_MAX_CONT_UNIT;
     }
 
     /* vmx:uuid.bios -> def:uuid */
@@ -1729,7 +1731,7 @@ virVMXParseConfig(virVMXContext *ctx,
         if (! present)
             continue;
 
-        for (unit = 0; unit < 16; ++unit) {
+        for (unit = 0; unit < def->scsiBusMaxUnit; unit++) {
             g_autoptr(virDomainDiskDef) disk = NULL;
 
             if (unit == 7) {
@@ -2164,7 +2166,8 @@ virVMXParseDisk(virVMXContext *ctx, virDomainXMLOption *xmlopt, virConf *conf,
      *                    VIR_DOMAIN_DISK_DEVICE_LUN}
      *         busType = VIR_DOMAIN_DISK_BUS_SCSI
      * controllerOrBus = [0..3] -> controller
-     *            unit = [0..6,8..15]
+     *            unit = [0..6,8..15] for virtualHW_version < 13
+     *            unit = [0..6,8..64] for virtualHW_version >= 13
      *
      *          device = {VIR_DOMAIN_DISK_DEVICE_DISK,
      *                    VIR_DOMAIN_DISK_DEVICE_CDROM,
@@ -2230,10 +2233,10 @@ virVMXParseDisk(virVMXContext *ctx, virDomainXMLOption *xmlopt, virConf *conf,
                 goto cleanup;
             }
 
-            if (unit < 0 || unit > 15 || unit == 7) {
+            if (unit < 0 || unit > vmdef->scsiBusMaxUnit || unit == 7) {
                 virReportError(VIR_ERR_INTERNAL_ERROR,
-                               _("SCSI unit index %d out of [0..6,8..15] range"),
-                               unit);
+                               _("SCSI unit index %d out of [0..6,8..%u] range"),
+                               unit, vmdef->scsiBusMaxUnit);
                 goto cleanup;
             }
 

src/conf/domain_conf.h
@@ -2727,6 +2727,7 @@ struct _virDomainVirtioOptions {
 };
 
 
+#define SCSI_SUPER_WIDE_BUS_MAX_CONT_UNIT 64
 #define SCSI_WIDE_BUS_MAX_CONT_UNIT 16
 #define SCSI_NARROW_BUS_MAX_CONT_UNIT 7
 

tests/vmx2xmldata/vmx2xml-esx-in-the-wild-11.xml
@@ -20,6 +20,11 @@
       <target dev='sda' bus='scsi'/>
       <address type='drive' controller='0' bus='0' target='0' unit='0'/>
     </disk>
+    <disk type='file' device='disk'>
+      <source file='[datastore] directory/esx6.7-rhel7.7-x86_64_3.vmdk'/>
+      <target dev='sdp' bus='scsi'/>
+      <address type='drive' controller='0' bus='0' target='0' unit='16'/>
+    </disk>
     <controller type='scsi' index='0' model='vmpvscsi'/>
     <interface type='bridge'>
       <mac address='00:50:56:ac:3e:a1' type='generated'/>

commit e2c7549d52e4520156d33320c02e6f4d4f0c1afe

vmx: Generate correct disk target device ID for superwide SCSI
Commit 32f7db0989e4 added support for superwide SCSI, but did not change
the disk ID calculation which resulted in a possible duplicate.  Change
it to calculate based on the (already decided) maximum of SCSI units per
bus and add a (well, modify existing) test case.

Fixes: 32f7db0989e4
Signed-off-by: Martin Kletzander <mkletzan@redhat.com>
Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>

src/vmx/vmx.c
@@ -2244,7 +2244,7 @@ virVMXGenerateDiskTarget(virDomainDiskDef *def,
             return -1;
         }
 
-        idx = controllerOrBus * 15 + (unit < 7 ? unit : unit - 1);
+        idx = controllerOrBus * (vmdef->scsiBusMaxUnit - 1) + (unit < 7 ? unit : unit - 1);
         prefix = "sd";
         break;
 

tests/vmx2xmldata/esx-in-the-wild-11.vmx
@@ -89,3 +89,9 @@ sched.scsi0:16.shares = "normal"
 sched.scsi0:16.throughputCap = "off"
 scsi0:16.present = "TRUE"
 scsi0:16.redo = ""
+scsi1.virtualDev = "pvscsi"
+scsi1.present = "TRUE"
+scsi1:0.deviceType = "scsi-hardDisk"
+scsi1:0.fileName = "esx6.7-rhel7.7-x86_64_4.vmdk"
+sched.scsi1:0.shares = "normal"
+scsi1:0.present = "TRUE"

tests/vmx2xmldata/esx-in-the-wild-11.xml
@@ -25,7 +25,13 @@
       <target dev='sdp' bus='scsi'/>
       <address type='drive' controller='0' bus='0' target='0' unit='16'/>
     </disk>
+    <disk type='file' device='disk'>
+      <source file='[datastore] directory/esx6.7-rhel7.7-x86_64_4.vmdk'/>
+      <target dev='sdbl' bus='scsi'/>
+      <address type='drive' controller='1' bus='0' target='0' unit='0'/>
+    </disk>
     <controller type='scsi' index='0' model='vmpvscsi'/>
+    <controller type='scsi' index='1' model='vmpvscsi'/>
     <interface type='bridge'>
       <mac address='00:50:56:ac:3e:a1' type='generated'/>
       <source bridge='VM Network'/>