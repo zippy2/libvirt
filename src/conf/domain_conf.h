4c999a24a86ada9ad603e8600beaa96588f8a0da
Michal Privoznik <mprivozn@redhat.com>
2026-06-04 15:10:15 +0200

    domain_conf: Introduce parsing and formating of virDomainGraphicsDef private data
    The virDomainGraphicsDef structure has privateData member which
    allow hypervisor drivers to store additional (runtime) state.
    For instance, the QEMU driver stores PID of a qemu-vnc process
    there (qemuDomainGraphicsPrivate::vnc::pid).
    
    Introduce mechanism for this data to be formatted into and parsed
    from domain status XML, just like we're already doing for other
    devices (e.g. TPMs).
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/conf/domain_conf.h
+++ src/conf/domain_conf.h
@@ -3644,6 +3644,11 @@ typedef int (*virDomainXMLPrivateDataNetParseFunc)(xmlXPathContextPtr ctxt,
 typedef int (*virDomainXMLPrivateDataNetFormatFunc)(const virDomainNetDef *net,
                                                     virBuffer *buf);
 
+typedef int (*virDomainXMLPrivateDataGraphicsParseFunc)(xmlXPathContextPtr ctxt,
+                                                        virDomainGraphicsDef *def);
+typedef int (*virDomainXMLPrivateDataGraphicsFormatFunc)(virBuffer *buf,
+                                                         const virDomainGraphicsDef *def);
+
 struct _virDomainXMLPrivateDataCallbacks {
     virDomainXMLPrivateDataAllocFunc  alloc;
     virDomainXMLPrivateDataFreeFunc   free;
@@ -3657,6 +3662,8 @@ struct _virDomainXMLPrivateDataCallbacks {
     virDomainXMLPrivateDataNewFunc    vsockNew;
     virDomainXMLPrivateDataNewFunc    cryptoNew;
     virDomainXMLPrivateDataNewFunc    graphicsNew;
+    virDomainXMLPrivateDataGraphicsParseFunc graphicsParse;
+    virDomainXMLPrivateDataGraphicsFormatFunc graphicsFormat;
     virDomainXMLPrivateDataNewFunc    hostdevNew;
     virDomainXMLPrivateDataNewFunc    networkNew;
     virDomainXMLPrivateDataNetParseFunc networkParse;
