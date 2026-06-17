ae60d414a727f2f662a0cb9c04244db373f21704
Michal Privoznik <mprivozn@redhat.com>
2026-06-17 17:11:18 +0200

    conf: Introduce virDomainDeviceAliasIndex()
    This is a driver agnostic version of qemuDomainDeviceAliasIndex()
    and chDomainDeviceAliasIndex(). To avoid code duplication, move
    it into a driver agnostic location.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/conf/domain_conf.c
+++ src/conf/domain_conf.c
@@ -5950,6 +5950,25 @@ virDomainDeviceAliasIsUserAlias(const char *aliasStr)
 }
 
 
+int
+virDomainDeviceAliasIndex(const virDomainDeviceInfo *info,
+                          const char *prefix)
+{
+    int idx;
+
+    if (!info->alias)
+        return -1;
+
+    if (!STRPREFIX(info->alias, prefix))
+        return -1;
+
+    if (virStrToLong_i(info->alias + strlen(prefix), NULL, 10, &idx) < 0)
+        return -1;
+
+    return idx;
+}
+
+
 static int
 virDomainDeviceInfoParseXML(virDomainXMLOption *xmlopt,
                             xmlNodePtr node,
